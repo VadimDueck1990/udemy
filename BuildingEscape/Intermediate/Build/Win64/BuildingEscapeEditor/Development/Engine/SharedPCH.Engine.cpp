bc24340315aea720cfd88df1818da741a1d1729c
unknown <vadim.dueck@mail.de>
2020-02-20 19:24:00 +0100

    initial commit, hopefully with correct .gitignore

---
+++ BuildingEscape/Intermediate/Build/Win64/BuildingEscapeEditor/Development/Engine/SharedPCH.Engine.cpp
@@ -0,0 +1,1 @@
+#include "C:/Users/vdc/workspace/unreal/udemy/BuildingEscape/Intermediate/Build/Win64/BuildingEscapeEditor/Development/Engine/SharedPCH.Engine.h"
---
+++ BuildingEscape/Source/BuildingEscape/WorldPosition.cpp
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WorldPosition.h"
+#include "GameFramework/Actor.h"
+
+// Sets default values for this component's properties
+UWorldPosition::UWorldPosition()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UWorldPosition::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	//// Logging Example with intro to Pointer
+	//FString Log = TEXT("Hello!");
+	//FString* PtrLog = &Log;
+
+	//Log.Len();		// Length of Log
+	//PtrLog->Len();	// same
+
+	//UE_LOG(LogTemp, Warning, TEXT("%s"), **PtrLog);	// dereferencing twice
+
+	FString Name = GetOwner()->GetName();
+	FString* PtrName = &Name;
+
+	UE_LOG(LogTemp, Warning, TEXT("This belongs to %s"), **PtrName);
+}
+
+
+// Called every frame
+void UWorldPosition::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
